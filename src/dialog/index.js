bef432101caa7c5fa553f7c443b39f2ccfeee007
brainpoint <bpoint.lee@gmail.com>
2021-10-14 18:57:11 +0800

    0.0.39

--- src/dialog/index.js
+++ src/dialog/index.js
@@ -3,6 +3,7 @@ import Vue from "vue";
 import * as febs from 'febs-browser';
 
 import * as dialog from './dialog';
+import * as popover from './popover';
 
 export * from './actionsheet';
 export * from './popover';
@@ -14,6 +15,8 @@ function injectGlobal() {
     return;
 
   Vue.component('ui-dialog', dialog.UIDialog);
+  Vue.component('ui-popover', popover.UIPopover);
+  Vue.component('ui-popover-cell', popover.UIPopoverCell);
   
   injectGlobal.installed = true;
 }
